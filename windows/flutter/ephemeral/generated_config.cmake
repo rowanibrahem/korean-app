b055fcdabe024b2795dfd8dac3f39a671fbaf5ef
rowan <90569337+rowanibrahem@users.noreply.github.com>
2023-03-17 10:27:19 -0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\college\\flutter\\lebonaapp" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\src\\flutter"
+  "PROJECT_DIR=E:\\college\\flutter\\lebonaapp"
+  "FLUTTER_ROOT=D:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\college\\flutter\\lebonaapp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\college\\flutter\\lebonaapp"
+  "FLUTTER_TARGET=E:\\college\\flutter\\lebonaapp\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\college\\flutter\\lebonaapp\\.dart_tool\\package_config.json"
+)
